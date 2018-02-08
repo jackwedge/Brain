dd00fdbeff2fb01fecae81e8cf7cea4435c267c8
Unknown <jackwedge@gmail.com>
2018-02-08 12:19:43 -0500

    started work on puzzle 3
    still need to fix releasing key

---
+++ Assets/puzzle_3 ->2_8_18/keyboardcontroller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keyboardcontroller : MonoBehaviour {
+
+	Sprite key_14;
+	public Sprite[] keyboardsprites;
+	public SpriteRenderer keyboardSpriterenderer;
+	// Use this for initialization
+
+	void Start () {
+		keyboardSpriterenderer = GetComponent<SpriteRenderer> ();
+		key_14 = keyboardSpriterenderer.sprite;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+	}
+
+	public void MousePressed(int keynumber){
+		keyboardSpriterenderer.sprite = keyboardsprites [keynumber];
+//		if (!MousePressed()){
+//			key_14 = keyboardSpriterenderer.sprite;
+
+
+		}
+}
+
---
+++ Assets/puzzle_3 ->2_8_18/keypressedcontroller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keypressedcontroller : MonoBehaviour {
+
+	public keyboardcontroller mymanager;
+	public int mykeynumber;
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnMouseDown (){
+		Debug.Log ("pianoplaying" + mykeynumber);
+		mymanager.MousePressed (mykeynumber);
+
+	}
+
+	void
+}
